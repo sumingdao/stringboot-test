801451b865b3656cdbe30cf38a8c080fa3f63c11
sumingdao <1041938729@qq.com>
2019-04-23 17:36:53 +0800

    修改了一个文件

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,14 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Console;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String a="1";
 	}
-
-
-
 
 }
